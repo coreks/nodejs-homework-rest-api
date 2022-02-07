0fe34fff19de82217a37cf22d0141169d94027e5
Aleksey Sobchenko <aleksobchenko@gmail.com>
2022-02-07 21:58:41 +0200

    Add authorization, add authorization validation

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -7,16 +7,22 @@ const { contacts: ctrl } = require("../../controllers");
 
 router.get("/", auth, ctrlWrapper(ctrl.getAllContacts));
 
-router.get("/:contactId", ctrlWrapper(ctrl.getContactById));
+router.get("/:contactId", auth, ctrlWrapper(ctrl.getContactById));
 
 router.post("/", auth, validation(joiSchema), ctrlWrapper(ctrl.postContact));
 
-router.delete("/:contactId", ctrlWrapper(ctrl.deleteContact));
+router.delete("/:contactId", auth, ctrlWrapper(ctrl.deleteContact));
 
-router.put("/:contactId", validation(joiSchema), ctrlWrapper(ctrl.putContact));
+router.put(
+  "/:contactId",
+  auth,
+  validation(joiSchema),
+  ctrlWrapper(ctrl.putContact)
+);
 
 router.patch(
   "/:contactId/favorite",
+  auth,
   validation(favoriteJoiSchema),
   ctrlWrapper(ctrl.updateFavorite)
 );
